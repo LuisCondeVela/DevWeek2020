8015d31a4d8074ed25e67595e4cfff4710b34e50
Luis <Luis@DESKTOP-DHCQBL6>
2020-02-05 16:23:41 -0400

    Ej 2 y 3 completados

---
+++ gradleproject1/src/main/java/Jala2020/Ejercicio3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jala2020;
+
+/**
+ *
+ * @author Luis
+ */
+public class Ejercicio3 {
+    
+    public Ejercicio3(){
+        
+    }
+    
+    public String ConvertirBinario(int n){
+        return Integer.toBinaryString(n);
+    }
+    
+}
